5b78cdc8c05fafe43b90562b19087df8ff7165a9
Benoit Jeaurond <benoit@jeaurond.dev>
2025-05-20 19:51:09 -0400

    feat: sort i18n keys

--- frontend/src/i18n/tools/unusedKeys.ts
+++ frontend/src/i18n/tools/unusedKeys.ts
@@ -3,7 +3,9 @@ import { promises as fs } from "fs";
 import { fileURLToPath } from "url";
 
 import { languages } from "@/i18n/lang";
-import ignoredKeys from "@/i18n/ignoredKeys.json";
+// import ignoredKeys from "@/i18n/ignoredKeys.json";
+
+const ignoredKeys: string[] = [];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
