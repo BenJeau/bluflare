87c720a1f14b08b8ca51f37db11ca67d898394ae
Benoit Jeaurond <benoit@jeaurond.dev>
2025-05-20 20:30:40 -0400

    chore: be able to restrict the t return type if no additional parameter passed

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -10,34 +10,45 @@ const valueRegex = /{([^}]+)}/;
 export const useTranslation = () => {
   const [lang, setLang] = useAtom(languageAtom);
 
+  // Used to retrict the type to a string when no values are provided
+  function tHelper(id: TransId): string;
+  function tHelper(
+    id: TransId,
+    values: Record<string, React.ReactNode>,
+  ): (string | React.ReactNode)[];
+
+  function tHelper(
+    id: TransId,
+    values?: Record<string, React.ReactNode>,
+  ): string | (string | React.ReactNode)[] {
+    const text = languages[lang].file[id];
+
+    if (
+      !values ||
+      Object.values(values).length === 0 ||
+      !valueRegex.test(text)
+    ) {
+      return text;
+    }
+
+    return text.split(valueRegex).map((part) => {
+      if (part in values!) {
+        return values![part];
+      }
+      return part;
+    });
+  }
+
   const otherLang: Language = useMemo(() => {
     const other = languages[lang].other;
     return { code: other, lang: languages[other] };
   }, [lang]);
-  const t = useCallback(
-    (id: TransId, values?: Record<string, React.ReactNode>) => {
-      const text = languages[lang].file[id];
 
-      if (
-        !values ||
-        Object.values(values).length === 0 ||
-        !valueRegex.test(text)
-      ) {
-        return text;
-      }
-
-      return text.split(valueRegex).map((part) => {
-        if (part in values) {
-          return values[part];
-        }
-        return part;
-      });
-    },
-    [lang],
-  );
   const toggle = useCallback(() => {
     setLang((prev) => languages[prev].other);
   }, [setLang]);
+
+  const t = useCallback(tHelper, [lang]);
 
   return { lang: languages[lang], t, toggle, otherLang };
 };
